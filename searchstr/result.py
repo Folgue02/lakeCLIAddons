8173256cde3288795aa0818e2d1d641ab9a1a4eb
Folgue02 <davidfolru@gmail.com>
2021-05-30 08:18:17 +0200

    searchstr addon remade

---
+++ searchstr/result.py
@@ -0,0 +1,43 @@
+import os
+from colorama import init
+from termcolor import colored
+init() # Colorama
+
+
+class match:
+    def __init__(self, whole_line:str, line_num:int, pattern:str, file:str):
+        self.whole_line = whole_line
+        self.line_num = line_num
+        self.pattern = pattern
+        self.file = file
+
+    def embed_result(self, color="green"):
+        return self.whole_line.replace(self.pattern, colored(self.pattern, color))
+
+
+
+class str_search:
+    def __init__(self, pattern, target_file):
+        self.pattern = pattern
+        self.target_file = target_file
+
+        if not os.path.exists(self.target_file):
+            raise FileNotFoundError(f"File '{self.target_file}' doesn't exist.")
+    
+        # Read file
+        self.target_content = open(self.target_file, "r").read()
+        self.matches = []
+
+        self.get_matches()
+
+
+    def get_matches(self) -> dict: 
+        # Finds the matches of a pattern compared to the lines to an specified file.
+        # Returns a dictionary with the matches
+        for index, line in enumerate(self.target_content.split("\n")):
+            if self.pattern in line:
+                self.matches.append(match(line, index+1, self.pattern, self.target_file))
+
+            else:
+                continue
+
--- searchstr/searchstr.py
+++ searchstr/searchstr.py
@@ -1,42 +1,93 @@
-#!/usr/bin/env python3
 import argparse
 import os
-from termcolor import colored
 from colorama import init
 init()
+from termcolor import colored
+from result import str_search, match
+from pathlib import Path
+import mimetypes
+from copy import deepcopy as dp
+
+def error(*args):
+    for a in args:
+        print(f"{colored('error:', 'red')}: {a}")
+
+
+def display_match(match:match, lines:bool, file_name:bool):
+    line_buffer = ""
+
+    if lines:
+        line_buffer += f"[{match.line_num}]"
+
+    if file_name:
+        line_buffer += colored(os.path.join(".", Path(match.file)), "magenta")
+
+    print((line_buffer + " " if line_buffer!="" else "") + match.embed_result())
+
+
+def iterate_dirs(arguments:argparse.ArgumentParser) -> None:
+    try:
+        elements = os.listdir(arguments.target)
+    except Exception:
+        error("Cannot inspect folder: " + arguments.target)
+        return
+
+    for x in elements:
+        
+        # Read file
+        if os.path.isfile(os.path.join(arguments.target, x)):
+            result = str_search(arguments.pattern, os.path.join(arguments.target, x))
+
+            for r in result.matches:
+                display_match(r, arguments.lines, os.path.join(arguments.target, x))
+
+        # Recursion
+        elif os.path.isdir(os.path.join(arguments.target, x)):
+            # Create a copy of the arguments
+            copy = dp(arguments)
+            copy.target = os.path.join(arguments.target, x)
+
+            # Do the same with another directory
+            iterate_dirs(copy)
+
+        else:
+            print(os.path.exists(os.path.join(arguments.target, x)))
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="File where to search the string from.", type=str)
-    parser.add_argument("targetString", help="String to search.", type=str)
+    
+    # Arguments
+    parser.add_argument("pattern", type=str, help="Pattern to search for")
+    parser.add_argument("target", type=str, help="Target to look for the pattern")
+    parser.add_argument("-l", "--lines",  action="store_true", help="Display line numbers")
+    parser.add_argument("-n", "--filename",  action="store_true", help="Display file name")
 
-    args = parser.parse_args()
-url
-    if not os.path.isfile(args.file):
-        print(f"The file '{args.file}' doesn't exist.")
+    arguments = parser.parse_args()
+
+    # Check target existence
+    if not os.path.exists(arguments.target):
+        error(f"Path '{os.path.abspath(arguments.target)}' doesn't exist.")
 
     else:
-        # Search for the string in the content of the file
+        # Check single file
+        if os.path.isfile(arguments.target):
+            try:
+                result = str_search(arguments.pattern, arguments.target)
+            except UnicodeDecodeError:
+                error("Cannot decode file '{arguments.target}'.")
 
-        try:
-            content = open(args.file, "r").read().split("\n") # Split the file in lines
+            for r in result.matches:
+                display_match(r, arguments.lines, arguments.filename)
+
         
-        except PermissionError:
-            print(colored(f"You don't have permissions to read this file: '{args.file}'", "red"))
+        elif os.path.isdir(arguments.target):
+            iterate_dirs(arguments)
 
-        locations = []
-
-        for index, line in enumerate(content):
-            if args.targetString in line:
-                locations.append([index+1, line])
-
-        for x in locations:
-            print(f"[{'0'*(len(str(len(content))) - len(str(x[0])))}{x[0]}]  {x[1].replace(args.targetString, colored(args.targetString, on_color='on_green'))}")
-        
-
-        if locations == []:
-            print(f"[{colored('X', 'red')}]" + f"'{args.targetString}' doesn't appear in the file.")
+        else:
+            error("Invalid file type.")
 
 
 if __name__ == "__main__":
     main()
+
+
